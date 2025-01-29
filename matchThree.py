7ffe890ae459ef6e13972f8c3d11ab53b89a2ba0
ZacWang123 <Zac.W2002@yahoo.com>
2025-01-29 15:45:00 +1000

    tile highlighting

--- matchThree.py
+++ matchThree.py
@@ -1,6 +1,7 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame # type: ignore
+import random
 
 running = False
 
@@ -19,7 +20,7 @@ blockSize = 38
 gameRow = 10
 gameCol = 10
 
-# GREY = (65, 65, 65)
+WHITE = (255, 255, 255)
 PURPLE = (128, 0, 128)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -32,6 +33,11 @@ GRID_COLOUR = (100, 100, 100)
 
 COLOURS = [BACKGROUND_COLOUR, PURPLE, RED, GREEN, YELLOW, BLUE, ORANGE]
 
+def Initialise_Grid(gameGrid):
+    for col in range(gameCol):
+        for row in range(gameRow):
+            gameGrid[col][row] = random.randint(1, 6)
+    return gameGrid
                 
 def Draw_Grid():
     screen = pygame.display.set_mode((windowLength, windowWidth))
@@ -55,12 +61,24 @@ def Update_Grid(screen, gameGrid):
             realY = gridWidthStart + row * cellSize
             pygame.draw.rect(screen, COLOURS[gameGrid[col][row]], (realX + cellSize - blockSize, realY + cellSize - blockSize, blockSize, blockSize))
 
+def Highlight_Cell(screen, row, col):
+    realX = gridLengthStart + col * cellSize
+    realY = gridWidthStart + row * cellSize
+    pygame.draw.rect(screen, WHITE, (realX + cellSize - blockSize, realY + cellSize - blockSize, blockSize, blockSize), 5)
+    
+def Unhighlight_Cell(screen, gameGrid, row, col):
+    realX = gridLengthStart + col * cellSize
+    realY = gridWidthStart + row * cellSize
+    pygame.draw.rect(screen, COLOURS[gameGrid[col][row]], (realX + cellSize - blockSize, realY + cellSize - blockSize, blockSize, blockSize))
+     
 def Start_Game():
     global running, gameRow, gameCol
     gameGrid = [[0 for i in range(gameRow)] for j in range(gameCol)]
     clickedTiles = []
     
     screen = Draw_Grid()
+    gameGrid = Initialise_Grid(gameGrid)
+    Update_Grid(screen, gameGrid)
 
     while running:
         for event in pygame.event.get():
@@ -72,9 +90,12 @@ def Start_Game():
                 row = (mouseY - gridWidthStart) // cellSize
                 
                 if row >= 0 and row < gameRow and col >= 0 and col < gameCol:
-                    gameGrid[col][row] = 1
-                    Update_Grid(screen, gameGrid)
-
+                    if ((row, col) in clickedTiles):
+                        clickedTiles.remove((row, col))
+                        Unhighlight_Cell(screen, gameGrid, row, col)
+                    else:         
+                        clickedTiles.append((row, col))
+                        Highlight_Cell(screen, row, col)
 
         pygame.display.update()
 
